ae3254ceedcd1451bc2fb5a9fcc46bc7a14e8b55
Leo Gimenez <leogimenez.dev@gmail.com>
2024-07-15 10:14:19 -0300

    se arma estructura

--- TelegramPyApp.py
+++ TelegramPyApp.py
@@ -1,7 +1,20 @@
 import requests
 
-CHATID = ''
-BOTTOKRN = ''
-mensaje = ''
-requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': 'Resultado de la prueba:'})
-requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': mensaje})
+CHATID = 'tu_chat_id_aqui'
+BOTTOKEN = 'tu_bot_token_aqui'
+mensaje = 'Resultado de la prueba'
+
+# URL base de la API de Telegram
+url = f'https://api.telegram.org/bot{BOTTOKEN}/sendMessage'
+
+# Datos a enviar en la solicitud POST
+data = {
+    'chat_id': CHATID,
+    'text': mensaje
+}
+
+# Enviar el mensaje
+response = requests.post(url, data=data)
+
+# Imprimir la respuesta para verificar si se envió correctamente
+print(response.json())
