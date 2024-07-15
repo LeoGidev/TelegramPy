2564b070d161430132a8320e868ffac2dfdf7c7d
Leo Gimenez <leogimenez.dev@gmail.com>
2024-07-15 10:10:19 -0300

    se arma estructura

--- TelegramPyApp.py
+++ TelegramPyApp.py
@@ -2,5 +2,6 @@ import requests
 
 CHATID = ''
 BOTTOKRN = ''
+mensaje = ''
 requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': 'Resultado de la prueba:'})
-#requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': compDwn})
+requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': mensaje})
