04cf8902c16454a17d8ffeb06df78abaf3748da1
Leo Gimenez <leogimenez.dev@gmail.com>
2024-07-15 10:20:05 -0300

    se arma estructura

--- TelegramPyApp.py
+++ TelegramPyApp.py
@@ -6,11 +6,11 @@ class TelegramBot:
         self.chat_id = chat_id
         self.base_url = f'https://api.telegram.org/bot{self.bot_token}'
 
- def send_message(self, message):
-        url = f'{self.base_url}/sendMessage'
-        data = {
-            'chat_id': self.chat_id,
-            'text': message
-        }
-        response = requests.post(url, data=data)
-        return response.json()
+    def send_message(self, message):
+            url = f'{self.base_url}/sendMessage'
+            data = {
+                'chat_id': self.chat_id,
+                'text': message
+            }
+            response = requests.post(url, data=data)
+            return response.json()
