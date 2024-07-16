5e9bd9099ee7828820e488157051cbd1e4b3b238
Leo Gimenez <leogimenez.dev@gmail.com>
2024-07-16 09:37:18 -0300

    se trabaja en recibir comandos

--- TelegramPyApp.py
+++ TelegramPyApp.py
@@ -37,7 +37,7 @@ class TelegramBot:
             elif message_text == '/help':
                 self.send_message(chat_id, 'Comandos disponibles: /start, /help, /archivo')
             elif message_text == '/archivo':
-                # Ruta del archivo que quieres enviar
+                # Ruta del archivo a enviar 
                 file_path = 'ruta/al/archivo.xlsx'
                 self.send_document(chat_id, file_path)
             else:
