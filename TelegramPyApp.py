2abd4b7bc2ee0c320d41003b32fc8a3286130a3f
Leo Gimenez <leogimenez.dev@gmail.com>
2024-07-16 09:22:18 -0300

    se trabaja en recibir comandos

--- TelegramPyApp.py
+++ TelegramPyApp.py
@@ -1,11 +1,10 @@
 import requests
 
 class TelegramBot:
-    def __init__(self, bot_token, chat_id):
+    def __init__(self, bot_token):
         self.bot_token = bot_token
-        self.chat_id = chat_id
         self.base_url = f'https://api.telegram.org/bot{self.bot_token}'
-
+        
     def send_message(self, message):
         url = f'{self.base_url}/sendMessage'
         data = {
